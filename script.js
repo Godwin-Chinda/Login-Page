a3c025cd05dca380f681bc3ab00abd338b954c36
GODWIN CHINDA <godwinbadubulachim@gmail.com>
2024-03-30 09:12:12 +0100

    add script

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
-const login = document.getElementById("Login");
+const loginButton = document.getElementById("Login");
+
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+  
+    if (usernameValue === '') {
+      alert('Please enter your username.');
+      return;
+    }
+  
+    if (passwordValue === '') {
+      alert('Please enter your password.');
+      return;
+    }
+  
+    console.log(`Username: ${usernameValue}, Password: ${passwordValue}`);
+  });
